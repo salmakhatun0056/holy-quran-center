c06a2c77343eb385a0240d46cd2249f050269a7b
salmakhatun12345e <ummesalma0056@gmail.com>
2022-04-05 13:10:29 +0600

    chart-recheck

--- src/Components/DashBoard/DashBoard.js
+++ src/Components/DashBoard/DashBoard.js
@@ -1,23 +1,37 @@
-import MyBarChart from '../MyBarChart/MyBarChart';
-import MyLineChart from '../MyLineChart/MyLineChart';
-import MyPieChart from '../MyPieChart/MyPieChart';
+import { Bar, BarChart, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import useChart from '../../hooks/useChart';
 import './DashBoard.css'
 
 const DashBoard = () => {
+    const [data, setData] = useChart()
 
     return (
         <div className='chart-container'>
             <div>
                 <h2>Month Wise Sell</h2>
-                <MyLineChart></MyLineChart>
+                <LineChart height={300} width={600} data={data}>
+                    <Line dataKey={"sell"}></Line>
+                    <Tooltip></Tooltip>
+                    <XAxis dataKey="month"></XAxis>
+                    <YAxis></YAxis>
+                </LineChart>
             </div>
             <div>
                 <h2>Investment vs Revenue</h2>
-                <MyBarChart></MyBarChart>
+                <BarChart width={600} height={300} data={data}>
+                    <Bar dataKey={'investment'} fill={'#8884d8'}></Bar>
+                    <Tooltip></Tooltip>
+                    <XAxis dataKey="month"></XAxis>
+                    <YAxis></YAxis>
+                </BarChart>
             </div>
             <div>
                 <h2>Investment vs Revenue</h2>
-                <MyPieChart></MyPieChart>
+                <PieChart width={600} height={300}>
+                    <Pie data={data} dataKey="sell" nameKey="month" cx="50%" cy="50%" outerRadius={80} fill="#8884d8" />
+
+                    <Pie data={data} dataKey="sell" nameKey="month" cx="50%" cy="50%" innerRadius={95} outerRadius={120} fill="#82ca9d" label />
+                </PieChart>
             </div>
         </div>
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,7 +10,6 @@ const Home = () => {
                 <div className="home-detail">
                     <h1 className='home-title'>Your best choice is the<br /><span className='title-style'>HOLY QURAN</span></h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magnam provident nostrum quas inventore harum?</p>
-                    <button>Live Demo</button>
                 </div>
                 <div className="home-picture">
                     <img src="photo-1.jpg" alt="" />
@@ -18,7 +17,7 @@ const Home = () => {
             </div>
 
             <HomeReviewDetailContainer></HomeReviewDetailContainer>
-            <div className='all-reviews-link'>
+            <div className='see-all-reviews-link'>
                 <button ><Link to='/reviews'>See All Reviews</Link></button>
             </div>
 
